511eec0e881b53f769181b48504278223944cf02
sammykingx <sammy@sammykingx.com.ng>
2026-08-21 18:48:41 +0100

    feat(ui): implement complete coming soon page frontend

--- app/core/urls.py
+++ app/core/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", TemplateView.as_view(template_name="public/soon.html"), name="coming_soon"),
+    path("", TemplateView.as_view(template_name="public/coming-soon.html"), name="coming_soon"),
 ]
